e848f94bad0bd9d50b0629cea78d8a7d817d3d8a
Markus Kobligk <kobligk@sitegeist.de>
2017-02-21 10:18:11 +0100

    [TASK] Raise dependency versions for TYPO3 8.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,9 +20,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.3.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.99.99',
-			'extbase' => '6.2.0-7.99.99',
-			'fluid' => '6.2.0-7.99.99',
+			'typo3' => '6.2.0-8.5.99',
+			'extbase' => '6.2.0-8.5.99',
+			'fluid' => '6.2.0-8.5.99',
 		),
 		'conflicts' => array(
 		),
